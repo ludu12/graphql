5311137a8bacae0bf617acfea05170e455a3a4de
Luke Dunscombe <ldunscombe@leantechniques.com>
2019-11-12 16:09:35 -0600

    Adding express and graphql, docker compose file and migrations

---
+++ docker/flyway/migration/V1.0__create_users.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Users (
+    id INTEGER PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100)
+);
+
+
+INSERT INTO Users (id, first_name, last_name) VALUES ('1', 'User', 'A');
+INSERT INTO Users (id, first_name, last_name) VALUES ('2', 'User', 'B');
